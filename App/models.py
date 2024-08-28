083c53852efb5b1fe56872579feeb64fe248a098
suresh-27-09 <132741377+suresh-27-09@users.noreply.github.com>
2024-08-28 11:27:23 +0530

    Update models

--- App/models.py
+++ App/models.py
@@ -10,3 +10,20 @@ class Register(models.Model):
 
     def __str__(self):
         return self.uname 
+class Category(models.Model):
+    cid=models.AutoField(primary_key=True)
+    c_name=models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.c_name
+class Products(models.Model):
+    pid=models.AutoField(primary_key=True)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    p_name=models.CharField(max_length=255)
+    p_price=models.FloatField()
+    p_img=models.ImageField(upload_to='images/')
+    p_des=models.TextField()
+
+    def __str__(self):
+        return self.p_name
+
